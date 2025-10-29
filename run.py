4d1e41c02d6e3f6346f6ccb6d0486d223fe67c33
RohitAnkolnerkar <147256698+RohitAnkolnerkar@users.noreply.github.com>
2025-10-29 17:25:25 +0530

    Update run.py

--- run.py
+++ run.py
@@ -14,5 +14,5 @@ fastapi_app.mount("/flask", WSGIMiddleware(flask_app))
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(fastapi_app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("run:fastapi_app", host="0.0.0.0", port=port, reload=False)
